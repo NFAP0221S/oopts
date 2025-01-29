329a4a33480f5225386e750e0383fc5cd32761df
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2025-01-29 23:11:01 +0900

    Update app.ts

--- 1_class_interface/cl-interfaces-01-starting-setup/src/app.ts
+++ 1_class_interface/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+class Department {
+    name: string;
+
+    // constructor는 클래스를 인스턴스화 할 때 실행되는 메서드
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const accounting = new Department("Accounting");
+
+console.log(accounting.name);
